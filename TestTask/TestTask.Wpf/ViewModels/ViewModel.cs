1c4f2a09a59ba5e4270d00cbeb9ec3d5eeb4de49
joker-x7 <joker-x7@yandex.ru>
2017-03-17 18:10:30 +0500

    Implement view model

--- TestTask/TestTask.Wpf/ViewModels/ViewModel.cs
+++ TestTask/TestTask.Wpf/ViewModels/ViewModel.cs
@@ -1,13 +1,152 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using TestTask.Wpf.Entities;
+using TestTask.Wpf.Models;
 
 namespace TestTask.Wpf.ViewModels
 {
     public class ViewModel : ViewModelBase
     {
+        private IModel model;
+
+        private string selectedCity;
+        private DateTime? selectedDate;
+        private Possibility selectedPossibility;
+        private string infoMessage;
+
+        public ObservableCollection<MeteringRecord> MeteringRecords { get; set; }
+        public ICollection<Possibility> Possibilitys { get; set; }
+        public ICollection<string> PossibilitysCity { get; set; }
+        public ObservableCollection<Possibility> SelectedPossibilitys { get; set; }
+        public MeteringRecord SelectedRecord { get; set; }
+        public DateTime? SelectedDate
+        {
+            get
+            {
+                return selectedDate;
+            }
+            set
+            {
+                selectedDate = value;
+                selectedPossibility = SelectedPossibilitys.FirstOrDefault(x => x.Date.Date == selectedDate.Value.Date);
+                RaisePropertyChanged(() => SelectedDate);
+                RaisePropertyChanged(() => SelectedPossibility);
+            }
+        }
+        public Possibility SelectedPossibility
+        {
+            get
+            {
+                return selectedPossibility;
+            }
+            set
+            {
+                selectedPossibility = value;
+                selectedDate = SelectedPossibility?.Date;
+                RaisePropertyChanged(() => SelectedDate);
+                RaisePropertyChanged(() => SelectedPossibility);
+            }
+        }
+        public string SelectedCity
+        {
+            get
+            {
+                return selectedCity;
+            }
+            set
+            {
+                selectedCity = value;
+                //TODO Заменить это убожество!!!
+                LoadSelectedPossibilitys();
+                //var res = model.GetByCity(selectedCity);
+                //SelectedPossibilitys.Clear();
+                //foreach(var item in res)
+                //{
+                //    SelectedPossibilitys.Add(item);
+                //}
+                //SelectedPossibilitys = new ObservableCollection<Possibility>(model.GetByCity(selectedCity));
+            }
+        }
+        public string InfoMessage
+        {
+            get
+            {
+                return infoMessage;
+            }
+            set
+            {
+                infoMessage = value;
+                RaisePropertyChanged(() => InfoMessage);
+            }
+        }
+
+        public ICommand SaveCommand { get; private set; }
+
+        public ViewModel()
+        {
+            model = new Model();
+            MeteringRecords = new ObservableCollection<MeteringRecord>(model.GetAllRecords());
+            Possibilitys = model.GetAllPossibilitys();
+            PossibilitysCity = Possibilitys.Select(x => x.City).Distinct().ToList();
+            SelectedPossibilitys = new ObservableCollection<Possibility>();
+            SaveCommand = new RelayCommand(Save);
+        }
+
+        private void Save()
+        {
+            var result = model.SetDate(SelectedRecord?.Number, SelectedDate, SelectedCity);
+            switch (result)
+            {
+                case ESetDateResult.Ok:
+                    InfoMessage = "Запись произведена";
+                    LoadSelectedPossibilitys();
+                    LoadSelectedRecords();
+                    break;
+                case ESetDateResult.DateIsNull:
+                    InfoMessage = "Выберите дату";
+                    break;
+                case ESetDateResult.NumberIsNull:
+                    InfoMessage = "Выберите запись";
+                    break;
+                case ESetDateResult.NotFoundRecord:
+                    InfoMessage = "Запись с таким номером не найдена";
+                    break;
+                case ESetDateResult.NotFoundPossibility:
+                    InfoMessage = string.Format("В городе {0} на {1} запись не производится", SelectedCity, SelectedDate.Value.Date);
+                    break;
+                case ESetDateResult.LimitIsOver:
+                    InfoMessage = string.Format("В городе {0} на {1} лимит исчерпан", SelectedCity, SelectedDate.Value.Date);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void LoadSelectedPossibilitys()
+        {
+            var res = model.GetByCity(selectedCity);
+            SelectedPossibilitys.Clear();
+            foreach (var item in res)
+            {
+                SelectedPossibilitys.Add(item);
+            }
+        }
+
+        private void LoadSelectedRecords()
+        {
+            var res = model.GetAllRecords();
+            MeteringRecords.Clear();
+            foreach (var item in res)
+            {
+                MeteringRecords.Add(item);
+            }
+        }
     }
 }

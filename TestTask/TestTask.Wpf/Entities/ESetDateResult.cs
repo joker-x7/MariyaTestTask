707ed64059f9923313aa8970279c5c2e2f2b3b77
joker-x7 <joker-x7@yandex.ru>
2017-03-17 18:07:52 +0500

    Add enum for returned result for set date of metering record

---
+++ TestTask/TestTask.Wpf/Entities/ESetDateResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask.Wpf.Entities
+{
+    public enum ESetDateResult
+    {
+        Ok,
+        DateIsNull,
+        NumberIsNull,
+        NotFoundRecord,
+        NotFoundPossibility,
+        LimitIsOver,
+    }
+}

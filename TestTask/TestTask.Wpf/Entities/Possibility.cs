c5705f4b5e50ad56e4bb2878f765cd1672e6227a
joker-x7 <joker-x7@yandex.ru>
2017-03-17 18:08:35 +0500

    Add entitie "Possibility"

---
+++ TestTask/TestTask.Wpf/Entities/Possibility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask.Wpf.Entities
+{
+    public class Possibility
+    {
+        public string City { get; set; }
+        public DateTime Date { get; set; }
+        public int Limit { get; set; }
+    }
+}
